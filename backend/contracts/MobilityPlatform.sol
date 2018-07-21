f7464b8482957595508c5e51ebc8853fc904e1d6
Kowalczyk Dariusz <KOWALD@de.softlab.net>
2018-07-21 19:27:04 +0200

    formating corrected

--- backend/contracts/MobilityPlatform.sol
+++ backend/contracts/MobilityPlatform.sol
@@ -4,43 +4,43 @@ contract MobilityPlatform {
 
     enum Status {Booked, Proposed, Accepted, Paid, OfferDeclined}
 
-constructor() public {
-userAccountBalance[0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe] = 2000; //Demo user account
-}
-struct UsageRecord {
-uint offerId;
-address provider;
-address user;
-uint timeStarted;
-uint timeFinished;
-uint proposedPrice;
-uint paidPrice;
-Status status;
-}
+    constructor() public {
+        userAccountBalance[0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe] = 2000; //Demo user account
+    }
+    struct UsageRecord {
+        uint offerId;
+        address provider;
+        address user;
+        uint timeStarted;
+        uint timeFinished;
+        uint proposedPrice;
+        uint paidPrice;
+        Status status;
+    }
+            
+    mapping(uint => UsageRecord) usageRecords;
 
-mapping(uint => UsageRecord) usageRecords;
+    mapping(address => uint) userAccountBalance;
+   
+    mapping(address => uint) providerAccountBalance;
+            
+    function proposeServiceUsage(uint offerId, uint timeStarted, uint proposedPrice, address user) public {
+        address provider = msg.sender;
+        usageRecords[offerId] = UsageRecord(offerId, provider, user, timeStarted, 0, proposedPrice, 0, Status.Proposed);
+        emit ServiceUsageProposalSaved(offerId);
+    }
+        
+    //function acceptService(uint offerId) public {
+    //}
+    //function getUserBalance() public view  returns (uint balance){
+    //balance = userAccountBalance[msg.sender];
+    //}
+    //function finishServiceUsage(uint serviceId, uint timeFinished) public{
+    //}
+    //function executePayment() public{
+    //
+    //}
 
-mapping(address => uint) userAccountBalance;
-
-mapping(address => uint) providerAccountBalance;
-
-function proposeServiceUsage(uint offerId, uint timeStarted, uint proposedPrice, address user) public {
-address provider = msg.sender;
-usageRecords[offerId] = UsageRecord(offerId, provider, user, timeStarted, 0, proposedPrice, 0, Status.Proposed);
-emit ServiceUsageProposalSaved(offerId);
-}
-
-//function acceptService(uint offerId) public {
-//}
-//function getUserBalance() public view  returns (uint balance){
-//balance = userAccountBalance[msg.sender];
-//}
-//function finishServiceUsage(uint serviceId, uint timeFinished) public{
-//}
-//function executePayment() public{
-//
-//}
-
-event ServiceUsageProposalSaved(uint offerId);
+    event ServiceUsageProposalSaved(uint offerId);
 }
 
