b982ce04519f9781a1b45d134a3e4335c11c341d
Jakob Niggel <info@jakobniggel.de>
2018-07-21 20:32:30 +0200

    add part

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter_google_places_autocomplete/flutter_google_places_autocom
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+part 'home.dart';
+
 Future<http.Response> callAmadeus(path, params) async {
   Uri apiUrl = new Uri(
       scheme: 'https',
@@ -125,68 +127,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new FlatButton(
-              onPressed: () async {
-                Prediction p = await showGooglePlacesAutocomplete(
-                    context: context,
-                    apiKey: kGoogleApiKey,
-                    onError: (res) {
-                      homeScaffoldKey.currentState.showSnackBar(
-                          new SnackBar(content: new Text(res.errorMessage)));
-                    },
-                    mode: Mode.fullscreen,
-                    language: "de",
-                    components: [new Component(Component.country, "de")]);
-
-                displayPrediction(p, homeScaffoldKey.currentState);
-              },
-              child: new Text("Please enter your start location")),
-            new FlatButton(
-              onPressed: () async {
-                Prediction p = await showGooglePlacesAutocomplete(
-                    context: context,
-                    apiKey: kGoogleApiKey,
-                    onError: (res) {
-                      homeScaffoldKey.currentState.showSnackBar(
-                          new SnackBar(content: new Text(res.errorMessage)));
-                    },
-                    mode: Mode.fullscreen,
-                    language: "de",
-                    components: [new Component(Component.country, "de")]);
-
-                displayPrediction(p, homeScaffoldKey.currentState);
-              },
-              child: new Text("Please enter your target location")),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
-}
 
 Future<Null> displayPrediction(Prediction p, ScaffoldState scaffold) async {
   if (p != null) {
