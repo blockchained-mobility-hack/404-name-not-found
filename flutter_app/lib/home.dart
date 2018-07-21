f26c5ec6fe83be0c4320cb5fa7d16415dfb28065
Jakob Niggel <info@jakobniggel.de>
2018-07-21 21:17:55 +0200

    .

--- flutter_app/lib/home.dart
+++ flutter_app/lib/home.dart
@@ -1,6 +1,16 @@
 part of 'main.dart';
 
 class _MyHomePageState extends State<MyHomePage> {
+  int current_step = 0;
+
+  ListView buildSearchView() {
+    return ListView(
+          children: [
+            buildSearchButton('Please choose your start'),
+            buildSearchButton('Please choose your target'),
+          ],
+        );
+  }
 
   FlatButton buildSearchButton(text) {
     return FlatButton(
@@ -58,9 +68,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    List<Step> my_steps = [
+      new Step(
+          title: new Text("Step 1"),
+          content: buildSearchView(),
+          isActive: true),
+      new Step(
+          title: new Text("Step 2"),
+          content: new Text("World!"),
+          state: StepState.editing,
+          isActive: true),
+      new Step(
+          title: new Text("Step 3"),
+          content: new Text("Hello World!"),
+          isActive: true),
+    ];
 
-
-    return new Scaffold(
+    /*return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
@@ -70,6 +94,45 @@ class _MyHomePageState extends State<MyHomePage> {
             buildSearchButton('Please choose your target'),
           ],
         )
+    );*/
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Container(
+          child: new Stepper(
+        currentStep: this.current_step,
+        steps: my_steps,
+        type: StepperType.horizontal,
+        onStepTapped: (step) {
+          setState(() {
+            current_step = step;
+          });
+          print("onStepTapped : " + step.toString());
+        },
+        onStepCancel: () {
+          setState(() {
+            if (current_step > 0) {
+              current_step = current_step - 1;
+            } else {
+              current_step = 0;
+            }
+          });
+          print("onStepCancel : " + current_step.toString());
+        },
+        onStepContinue: () {
+          setState(() {
+            if (current_step < my_steps.length - 1) {
+              current_step = current_step + 1;
+            } else {
+              current_step = 0;
+            }
+          });
+          print("onStepContinue : " + current_step.toString());
+        },
+      )),
+
     );
   }
 }
