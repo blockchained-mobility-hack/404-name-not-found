e8ee0772e46ee60301e8c12f5543160611d98c8a
Rafal Hofman <rafhof91@gmail.com>
2018-07-22 13:36:02 +0200

    Leave only chalk for demo

--- service-provider-cli/serviceProviderCli.js
+++ service-provider-cli/serviceProviderCli.js
@@ -1,15 +1,10 @@
 'use strict'
 
-const program = require('commander')
 
 const chalkAnimation = require('chalk-animation');
 
 console.log("SN 234578 SERVICE PROVIDER NODE STARTED...")
-const animation = chalkAnimation.neon("SCANNING FOR DEVICES...");
-setTimeout(() => {
-    animation.stop(); // Animation stops
-    console.log("DEVICE WITH IP 85.183.197.59 FOUND")
-}, 4000);
+chalkAnimation.neon("SCANNING FOR DEVICES...");
 
 
 
