ce899e8f28f2b60d52b1c0b3422c0cd41dcdf323
Rafal Hofman <rafhof91@gmail.com>
2018-07-22 03:16:23 +0200

    Change blockchain api implementation

--- backend/blockchain/api.js
+++ backend/blockchain/api.js
@@ -16,7 +16,7 @@ async function initializeSmartContract() {
     await inializeWeb3()
     contractOwnerAddress = await initializeBlockchainAccount(contractOwnerPassword)
     await unlockBlockchainAccount(contractOwnerAddress, contractOwnerPassword)
-    await initializeContract(contractData.abi, contractData.bytecode, contractOwnerAddress, contractOwnerPassword)
+    deployedContract = await initializeContract(contractData.abi, contractData.bytecode, contractOwnerAddress, contractOwnerPassword)
     userBlockchainAddress = await initializeBlockchainAccount(userBlockchainPassword)
     serviceBlockchainAddress = await initializeBlockchainAccount(serviceBlockchainPassword)
 }
@@ -24,13 +24,13 @@ async function initializeSmartContract() {
 const inializeWeb3 = async () => {
     if (!web3) {
         const rpcUrl = "http://localhost:8545/"
-        web3 = new (Web3)(rpcUrl)
+        web3 = new Web3(rpcUrl)
     }
 }
 
 const initializeContract = async (abi, bytecode, ownerAddress, ownerPassword) => {
-    const testContract = new web3.eth.Contract(abi)
-    const deployTransaction = testContract.deploy({data: bytecode, arguments: []})
+    const contractInterface = new web3.eth.Contract(abi)
+    const deployTransaction = contractInterface.deploy({data: bytecode, arguments: []})
     const estimatedGas = await deployTransaction.estimateGas()
     return deployTransaction.send({gas: estimatedGas, from: contractOwnerAddress})
 }
@@ -43,12 +43,21 @@ const unlockBlockchainAccount = async (blockchainAccount, blockchainPassword) =>
     return web3.eth.personal.unlockAccount(blockchainAccount, blockchainPassword)
 }
 
+const getAccount = (accountType) => {
+    if (accountType === "user") {
+        return {blockchainAddress: userBlockchainAddress, blockchainPassword: userBlockchainPassword}
+    } else if (accountType === "service") {
+        return {blockchainAddress: serviceBlockchainAddress, blockchainPassword: serviceBlockchainPassword}
+    }
+}
+
+const getMobilityContract = () => {
+    return deployedContract
+}
+
 module.exports = {
     initializeSmartContract,
-    deployedContract,
-    userBlockchainPassword,
-    userBlockchainAddress,
-    serviceBlockchainPassword,
-    serviceBlockchainAddress,
+    getMobilityContract,
+    getAccount,
     unlockBlockchainAccount
 }
